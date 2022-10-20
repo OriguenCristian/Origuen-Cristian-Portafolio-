a8ce2ea0a9d354eece45dc29400b24965b455a27
Cristian Origuen <107323524+OriguenCristian@users.noreply.github.com>
2022-10-19 23:50:19 -0500

    Cristian Origuen

--- JS/index.js
+++ JS/index.js
@@ -3,20 +3,21 @@ const btnMenu = id("btnMenu")
 const label= id("btnMenuLabel");
 const img = id("imgLogo");
 const container = id("headerContainer");
+const liBtn = document.querySelector(".liBtn");
 const contenedorBtn = document.querySelector(".contenedorBotones");
 
-btnMenu.addEventListener('change', function() {
-    
-    if(window.scrollY>100){
-
-        contenedorBtn.style.display = this.checked ? "flex" :"";
-    }
-   
-  });
 
 function id(str){
     return document.getElementById(str);    
 }
+
+btnMenu.addEventListener('change', function(){
+    
+    if(window.scrollY>100){
+        contenedorBtn.style.display = this.checked ? "" :"flex";
+    }
+   
+  });
  
 function ScrollMenu(){ 
     contenedorBtn.classList.toggle("ScrollContenedorBtn",window.scrollY>100)
@@ -24,8 +25,9 @@ function ScrollMenu(){
     label.classList.toggle("scrollIconLabel", window.scrollY>100)
     if( window.scrollY>100){
         img.style.cssText ="display:none"
+       
         
     } else{
-        img.style.cssText ="display:"
+        img.style.cssText ="display: inline-bolock"
     }
 }
